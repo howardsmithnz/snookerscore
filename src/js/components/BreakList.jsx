c35ead992eac80a23be0c2db128705279617e90e
Howard Smith <howardsmithnz@gmail.com>
2016-05-31 18:06:06 +1200

    change of inline styles

--- src/js/components/BreakList.jsx
+++ src/js/components/BreakList.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default class BreakList extends React.Component {
   render () {
+    var divStyle = { background: 'green'}
     return (
-    <div>
+    <div style={divStyle}>
       <h3>Break Total: {this.props.currentBreakTotal}</h3>
       <h3>Break Sequence: {this.props.currentBreakSeq}</h3>
     </div>
--- src/js/components/EnterScore.jsx
+++ src/js/components/EnterScore.jsx
@@ -15,16 +15,47 @@ export default class EnterScore extends React.Component {
 
   render () {
     console.log('EnterScore props are:', this.props)
+    const buttonStyles = {
+      borderRadius: '50%'
+    }
     return (
     <div>
       'Enter the score here'
-      <input type="button" value="1" onClick={this.handleScoreChange(1)} />
-      <input type="button" value="2" onClick={this.handleScoreChange(2)} />
-      <input type="button" value="3" onClick={this.handleScoreChange(3)} />
-      <input type="button" value="4" onClick={this.handleScoreChange(4)} />
-      <input type="button" value="5" onClick={this.handleScoreChange(5)} />
-      <input type="button" value="6" onClick={this.handleScoreChange(6)} />
-      <input type="button" value="7" onClick={this.handleScoreChange(7)} />
+      <input
+        type="button"
+        style={buttonStyles}
+        value="1"
+        onClick={this.handleScoreChange(1)} />
+      <input
+        type="button"
+        style={buttonStyles}
+        value="2"
+        onClick={this.handleScoreChange(2)} />
+      <input
+        type="button"
+        style={buttonStyles}
+        value="3"
+        onClick={this.handleScoreChange(3)} />
+      <input
+        type="button"
+        style={buttonStyles}
+        value="4"
+        onClick={this.handleScoreChange(4)} />
+      <input
+        type="button"
+        style={buttonStyles}
+        value="5"
+        onClick={this.handleScoreChange(5)} />
+      <input
+        type="button"
+        style={buttonStyles}
+        value="6"
+        onClick={this.handleScoreChange(6)} />
+      <input
+        type="button"
+        style={buttonStyles}
+        value="7"
+        onClick={this.handleScoreChange(7)} />
       <br /> Change the player
       <input type="button" value="change player" onClick={this.handlePlayerChange.bind(this)} />
     </div>
